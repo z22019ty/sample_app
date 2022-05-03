4409f910485984d6039d32e9420bccbe44ee76e3
z22019ty@aiit.ac.jp <z22019ty@aiit.ac.jp>
2022-05-03 06:44:19 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,10 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
