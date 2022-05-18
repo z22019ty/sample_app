4469a6e1d922479113934be92c1f97e9a301b609
z22019ty@aiit.ac.jp <z22019ty@aiit.ac.jp>
2022-05-18 15:14:21 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-springs-59938.herokuapp.com'
+  host = 'https://quiet-springs-59938.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
