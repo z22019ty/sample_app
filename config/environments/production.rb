8c763a8ef211ddab778d6fe8a057e393583f53ab
z22019ty@aiit.ac.jp <z22019ty@aiit.ac.jp>
2022-05-18 15:21:23 +0000

    update develoment.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://quiet-springs-59938.herokuapp.com'
+  host = 'quiet-springs-59938.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
@@ -76,11 +76,7 @@ Rails.application.configure do
     :domain         => host,
     :authentication => :plain,
   }
-  .
-   
-   
-   
-   
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
